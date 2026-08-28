152572dd6fc791648ddd1b637af8ec5279bcd622
Matt Brock <matt@voxtelesys.com>
2026-08-28 10:38:48 -0500

    base node files

---
+++ nodes/Voxtelesys/Voxtelesys.node.ts
@@ -0,0 +1,25 @@
+import type { INodeTypeBaseDescription, IVersionedNodeType } from 'n8n-workflow';
+import { VersionedNodeType } from 'n8n-workflow';
+
+import { VoxtelesysV1 } from './v1/VoxtelesysV1';
+
+// Project is versioned by default in order to make future versioning simple
+export class Voxtelesys extends VersionedNodeType {
+	constructor() {
+		const baseDescription: INodeTypeBaseDescription = {
+			displayName: 'Voxtelesys',
+			name: 'voxtelesys',
+			icon: 'file:voxtelesys.svg',
+			group: ['output'],
+			subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+			description: 'Send SMS and MMS via Voxtelesys',
+			defaultVersion: 1,
+		};
+
+		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
+			1: new VoxtelesysV1(baseDescription),
+		}
+
+		super(nodeVersions, baseDescription)
+	}
+}
