79c0594a21983f304fddf40844cf8441154477fe
Matt Brock <matt@voxtelesys.com>
2026-09-14 13:06:57 -0500

    comma 2

--- nodes/Voxtelesys/Voxtelesys.node.ts
+++ nodes/Voxtelesys/Voxtelesys.node.ts
@@ -15,7 +15,7 @@ export class Voxtelesys extends VersionedNodeType {
 			},
 			group: ['output'],
 			subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
-			description: 'Send SMS, MMS and RCS messages via Voxtelesys',
+			description: 'Send SMS, MMS, and RCS messages via Voxtelesys',
 			defaultVersion: 1,
 		}
 
