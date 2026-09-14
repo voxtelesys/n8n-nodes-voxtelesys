c44fe9b1cbf345091aa6ffb5e989c6c80c05293d
Matt Brock <matt@voxtelesys.com>
2026-09-14 08:30:46 -0500

    new consts

--- nodes/Voxtelesys/v1/helpers/constants.ts
+++ nodes/Voxtelesys/v1/helpers/constants.ts
@@ -1,2 +1,10 @@
 // The maximum length accepted for `tag` and `bulk_tag` properties on Voxtelesys APIs
 export const MAX_TAG_LENGTH = 256
+
+// Limits the RCS API places on the suggestion chips attached to a message
+export const MAX_SUGGESTIONS = 11
+export const MAX_SUGGESTION_TEXT_LENGTH = 25
+export const MAX_CALLBACK_DATA_LENGTH = 2048
+export const MAX_LOCATION_LABEL_LENGTH = 100
+export const MAX_EVENT_TITLE_LENGTH = 100
+export const MAX_EVENT_DESCRIPTION_LENGTH = 500
